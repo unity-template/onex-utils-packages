9db840a280f9a6ded6d39031e4056df6a024ac70
云墨 <lg208143@alibaba-inc.com>
2021-01-25 22:20:33 +0800

    fix: 解决jest运行终止问题,fix #3

--- packages/build-plugin-utils/__tests__/build.spec.ts
+++ packages/build-plugin-utils/__tests__/build.spec.ts
@@ -3,16 +3,15 @@ import * as fs from 'fs-extra';
 import * as path from 'path';
 
 describe('simple build test suite', () => {
-  beforeAll(async () => {
-    return await build({
+  test('check output source', async () => {
+    await build({
       args: {},
       eject: false,
       rootDir: path.join(__dirname, 'fixtures/basic-utils/'),
       plugins: [path.join(__dirname, '../src/index.ts')],
       getBuiltInPlugins: () => [],
     });
-  });
-  test('check output source', () => {
     expect(fs.existsSync(path.join(__dirname, 'fixtures/basic-utils/build/index.js'))).toBe(true);
-  }, 30000);
+  });
 });
+
